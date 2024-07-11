c80e37abd81e491c5ab54337bb91dddab5f63e4c
risda29 <risdar187@gmail.com>
2024-07-11 16:59:33 +0800

    insert data

--- app/Controllers/Lokasi.php
+++ app/Controllers/Lokasi.php
@@ -1,9 +1,18 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\ModelLokasi;
 
 class Lokasi extends BaseController
 {
+    protected $ModelLokasi;
+
+    public function __construct()
+    {
+        // parent::__construct(); // Memanggil konstruktor parent
+        $this->ModelLokasi = new ModelLokasi();
+    }
+
     public function index()
     {
         $data = [
@@ -12,6 +21,7 @@ class Lokasi extends BaseController
         ];
         return view('v_template', $data);
     }
+
     public function inputlokasi()
     {
         $data = [
@@ -21,6 +31,62 @@ class Lokasi extends BaseController
         return view('v_template', $data);
     }
 
-
-
+    public function insertData()
+    {
+        if ($this->validate([
+            'nama_sekolah' => [
+                'label' => 'Nama Sekolah',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} Tidak Boleh Kosong'
+                ]
+            ],
+            'jenis_sekolah' => [
+                'label' => 'Jenis Sekolah',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} Tidak Boleh Kosong'
+                ]
+            ],
+            'latitude' => [
+                'label' => 'latitude',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} Tidak Boleh Kosong'
+                ]
+            ],
+            'longitude' => [
+                'label' => 'longitude',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} Tidak Boleh Kosong'
+                ]
+            ],
+            'foto_sekolah' => [
+                'label' => 'Foto Lokasi',
+                'rules' => 'uploaded[foto_sekolah]|max_size[foto_sekolah,1024]|mime_in[foto_sekolah,image/jpg,image/jpeg,image/png]',
+                'errors' => [
+                    'uploaded' => '{field} Tidak boleh kosong',
+                    'max_size' => 'Ukuran {field} maksimal 1024 kb',
+                    'mime_in' => 'Format {field} harus jpg, jpeg, png',
+                ]
+            ],
+        ])) {
+            $foto_sekolah = $this->request->getFile('foto_sekolah');
+            $nama_file_foto = $foto_sekolah->getRandomName();
+            $data = [
+                'nama_sekolah' => $this->request->getPost('nama_sekolah'),
+                'jenis_sekolah' => $this->request->getPost('jenis_sekolah'),
+                'latitude' => $this->request->getPost('latitude'),
+                'longitude' => $this->request->getPost('longitude'),
+                'foto_sekolah' => $nama_file_foto
+            ];
+            $foto_sekolah->move('foto', $nama_file_foto);
+            $this->ModelLokasi->insertData($data);
+            return redirect()->to('lokasi/index');
+            
+        } else {
+            return redirect()->to('lokasi/inputlokasi')->withInput();
+        }
+    }
 }
--- app/Views/lokasi/v_input_lokasi.php
+++ app/Views/lokasi/v_input_lokasi.php
@@ -5,24 +5,41 @@
             </div>
             <div class="col-sm-4">
                 <div class="row">
+                    <div class="row">
+                        <?php $errors = validation_errors() ?>
+                        <?php echo form_open_multipart('Lokasi/insertData') ?>
                     <div class="form-group">
                         <label>Nama Sekolah</label>
-                        <input class="form-control" name="nama_lokasi">
+                        <input class="form-control" name="nama_sekolah">
+                        <p class=" text-danger"><?= isset($erros['nama_sekolah']) == isset($erros['nama_sekolah']) ? validation_show_error('nama_sekolah') :'' ?></p>
                     </div>
                     <div class="form-group">
-                        <label>Alamat Sekolah</label>
-                        <input class="form-control" name="alamat_lokasi">
+                        <label>Jenis Sekolah</label>
+                        <input class="form-control" name="jenis_sekolah">
+                        <p class=" text-danger"><?= isset($erros['jenis_sekolah']) == isset($erros['jenis_sekolah']) ? validation_show_error('jenis_sekolah') :'' ?></p>
                     </div>
                     <div class="form-group">
                         <label>Latitude</label>
                         <input class="form-control" name="latitude">
+                        <p class=" text-danger"><?= isset($erros['latitude']) == isset($erros['latitude']) ? validation_show_error('latitude') :'' ?></p>
                     </div>
                     <div class="form-group">
                         <label>Longitude</label>
                         <input class="form-control" name="longitude">
+                        <p class=" text-danger"><?= isset($erros['longitude']) == isset($erros['longitude']) ? validation_show_error('longitude') :'' ?></p>
                     </div>
+                    <div class="form-group">
+                        <label>Foto Sekolah</label>
+                        <input type="file" class="form-control" name="foto_sekolah">
+                        <p class=" text-danger"><?= isset($erros['foto_sekolah']) == isset($erros['foto_sekolah']) ? validation_show_error('foto_sekolah') :'' ?></p>
+                    </div>
+                    
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                    <button type="batal" class="btn btn-danger">Batal</button>
+                    <?php echo form_close() ?>
                 </div>
             </div>
+        </div>
         </div>
     </div>
 
@@ -62,5 +79,34 @@ const baseLayers = {
      "Dark Map": peta4
 };
 const layerControl = L.control.layers(baseLayers).addTo(map);
+
+const getColor = (jenis) => {
+    switch(jenis) {
+        case 'negeri': return "#ff0000"; // Merah
+        case 'swasta': return "#00ff00"; // Hijau
+        case 'internasional': return "#0000ff"; // Biru
+        default: return "#ffffff"; // Putih sebagai default
+    }
+};
+
+$.getJSON("<?= base_url('geojson/sekolah.geojson') ?>", function(data){
+    L.geoJson(data, {
+        style: function(feature) {
+            return {
+                fillOpacity: 0.5,
+                weight: 3,
+                opacity: 1,
+                color: getColor(feature.properties.jenis)
+            };
+        },
+        onEachFeature: function(feature, layer) {
+            if (feature.properties && feature.properties.keterangan) {
+                layer.bindPopup(feature.properties.keterangan);
+            } else {
+                console.error("Property 'keterangan' not found in GeoJSON feature:", feature);
+            }
+        }
+    }).addTo(map);
+});
 
 </script>
